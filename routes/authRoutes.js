6179d493d58afa77f02afcda74faa05ca51bf33a
jaya <kjayashree061@gmail.com>
2024-12-05 18:43:59 +0530

    second commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,34 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const{register,login}=require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
+router.post('/register', register);
+router.post('/login', login);
 
-router.post('/register', async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newUser = new User({ name, email, password: hashedPassword });
-  
-      await newUser.save();
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error registering user', error });
-    }
-  });
-
-  router.post('/login', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-  
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
-  
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.status(200).json({ token });
-    } catch (error) {
-      res.status(500).json({ message: 'Error logging in', error });
-    }
-  });
 module.exports = router;
